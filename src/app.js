da52e10411477e2ec7a1bbfab6316b266df81b12
Nicolas Cesar Sabbatini Vrech <nicolas.sabbatini.95@gmail.com>
2022-04-27 10:48:49 -0300

    Bot andando

--- src/app.js
+++ src/app.js
@@ -2,11 +2,10 @@ require('dotenv').config();
 
 const { App } = require('@slack/bolt');
 const { default: axios } = require('axios');
-const moment = require('moment');
 
 const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  token: process.env.SLACK_BOT_TOKEN,
+  token: process.env.SLACK_BOT_OAUTH_TOKEN,
   customRoutes: [
     {
       path: '/',
@@ -25,5 +24,5 @@ app.message(':robot_face:', async ({ message, say }) => {
 
 (async () => {
   await app.start(process.env.PORT ? Number.parseInt(process.env.PORT, 10) : 3000);
-  console.log(`‚ö°Ô∏è Bolt app is running in port ${Number.parseInt(process.env.PORT)}!`);
+  console.log(`Bolt app is running in port ${Number.parseInt(process.env.PORT)}!`);
 })();
